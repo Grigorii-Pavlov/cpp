c822ace9eadef6732846563eb9c145825f1cadd1
Pavlov_G <Grigorij93@mail.ru>
2023-09-19 22:13:50 +0300

    Добавление удалённого файла 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Please enter two natural numbers:\n";
+	int one, two;
+	cin >> one >> two;
+	int sum = 0;
+	for (int number = one; number <= two; ++number) {
+		sum += number;
+    }
+
+	cout << "The sum of the range of common numbers = " << sum << "\n";
+
+	return 0;
+}
